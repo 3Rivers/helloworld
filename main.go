c75dd8d28c1eecf1472a8c540b32276c99dc692b
unknown <bingbing.he@sl.com>
2020-10-30 13:43:16 +0800

    添加etcd服务注册

--- main.go
+++ main.go
@@ -5,15 +5,27 @@ import (
 	"github.com/micro/go-micro/v2"
 	"github.com/3Rivers/helloworld/handler"
 	"github.com/3Rivers/helloworld/subscriber"
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/registry/etcd"
 
 	helloworld "github.com/3Rivers/helloworld/proto/helloworld"
 )
+
+var etcdReg registry.Registry
+
+func  init()  {
+	//新建一个consul注册的地址，也就是我们consul服务启动的机器ip+端口
+	etcdReg = etcd.NewRegistry(
+		registry.Addrs("192.168.2.254:12379", "192.168.2.254", "192.168.2.254"),
+	)
+}
 
 func main() {
 	// New Service
 	service := micro.NewService(
 		micro.Name("go.micro.service.helloworld"),
 		micro.Version("latest"),
+		micro.Registry(etcdReg),
 	)
 
 	// Initialise service
